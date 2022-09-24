97ce6daa69e5ad77ee16988990e14258fee2b5ea
Orpheous1 <orpheous18@yahoo.gr>
2022-09-24 17:37:38 +0200

    NMTDL4NLP-6 single teacher model implementation

--- distiller/Distiller.py
+++ distiller/Distiller.py
@@ -5,11 +5,13 @@ from torch import Tensor
 import torch.nn as nn
 from torch.nn import CrossEntropyLoss, KLDivLoss, CosineEmbeddingLoss
 from transformers import PretrainedConfig
-from transformers.models.marian.modeling_marian import MarianEncoder, MarianModel, MarianMTModel
+from transformers.models.t5.modeling_t5 import T5ForConditionalGeneration
+import pytorch_lightning as pl
+from torch.optim import Adam
 
 "Heavily influenced from https://gist.github.com/remi-or/4814577c59f4f38fcc89729ce4ba21e6"
 
-def create_student_model(teacher: Module,
+def _create_student_model(teacher: Module,
                          n: int):
     """Create a student model from a teacher model.
 
@@ -21,45 +23,62 @@ def create_student_model(teacher: Module,
         A student model.
     """
     config = teacher.config.to_dict()
-    config['num_hidden_layers'] //= n
+    config['num_layers'] //= n
+    config['num_decoder_layers'] //= n
     config = PretrainedConfig.from_dict(config)
     student_model = type(teacher)(config)
     student = student_model
     student.n = n
-    init_student_weights_DFS(teacher, student)
+    _init_student_weights(teacher, student)
     return student
 
-def init_student_weights_DFS(teacher: Module,
-                         student: Module):
+def _init_student_weights(teacher: Module,
+                          student: Module):
     """Initialize the weights of a student model.
 
     Args:
         student: The student model.
         teacher: The teacher model.
     """
-    if isinstance(teacher, MarianModel) or type(teacher).__name__.startswith('MarianNMT'):
-        for t, s in zip(teacher.children(), student.children()):
-            init_student_weights_DFS(t, s)
-    elif isinstance(teacher, MarianEncoder):
-        t_enc_layers = [l for l in next(teacher.children())]
-        s_enc_layers = [l for l in next(student.children())]
-        for i in range(len(s_enc_layers)):
-            s_enc_layers[i].load_state_dict(t_enc_layers[i * student.n].state_dict())
-    else:
-        student.load_state_dict(teacher.state_dict())
+    student.shared.weight.data = teacher.shared.weight.data
+    student.encoder.final_layer_norm.weight.data = teacher.encoder.final_layer_norm.weight.data
+    student.decoder.final_layer_norm.weight.data = teacher.decoder.final_layer_norm.weight.data
+    # Encoder
+    for i in range(student.config.num_layers):
+        student.encoder.block[i].load_state_dict(teacher.encoder.block[i * student.n].state_dict())
+    # Decoder
+    for i in range(student.config.num_decoder_layers):
+        student.decoder.block[i].load_state_dict(teacher.decoder.block[i * student.n].state_dict())
+
+class Teacher(pl.LightningModule):
+    def __init__(self, model: T5ForConditionalGeneration):
+        super().__init__()
+        self.model = model
+        self.model.eval()
+
+    def forward(self, input_ids: Tensor,
+                attention_mask: Tensor,
+                decoder_input_ids: Tensor,
+                decoder_attention_mask: Tensor) -> Tensor:
+        return self.model(input_ids=input_ids,
+                          attention_mask=attention_mask,
+                          decoder_input_ids=decoder_input_ids,
+                          decoder_attention_mask=decoder_attention_mask,
+                          labels=decoder_input_ids)
 
 
-class Distiller:
-    """
-    Distiller class for initializing a student model
-    """
-
+    def training_step(self, batch: Tuple[Tensor, Tensor, Tensor], batch_idx: int) -> Tensor:
+        pass
+class Distiller(pl.LightningModule):
     def __init__(self,
-                 teacher: MarianMTModel,
+                 teacher: T5ForConditionalGeneration,
                  n: int,
                  temperature: float = 1.0,
-                 loss_weights: list[float] = [1/3, 1/3, 1/3]
-                 ):
+                 loss_weights: list[float] = [1/3, 1/3, 1/3],
+                 lr: float = 2e-5,
+                 weight_decay=0.01,
+                 model_name: str = 't5-small',
+                 **kwargs):
         """
         Args:
             teacher: The teacher model.
@@ -68,33 +87,32 @@ class Distiller:
             loss_weights: The weights to use for the loss.
                 loss_weights format:
                     [CE, KL, Cosine]
+            lr: The learning rate
+            kwargs: Additional arguments
         """
-
-        self.teacher = teacher
-        self.student = create_student_model(teacher, n)
+        super().__init__()
+        self.save_hyperparameters()
+        self.teacher = Teacher(teacher)
+        self.student = _create_student_model(teacher, n)
+        self.teacher.freeze()
         self.n = n
         self._temperature = temperature
         self.loss_weights = loss_weights
         assert len(self.loss_weights) == 3, "loss_weights must be a list of length 3"
-        assert torch.allclose(torch.tensor(1), torch.tensor(self.loss_weights).sum()), "loss_weights must sum to 1"
-
-    @property
-    def temperature(self):
-        return self._temperature if self.training else 1
-
-    @temperature.setter
-    def temperature(self,
-                    value: float):
-        if temperature < 1:
-            raise ValueError('Temperature must be greater than 1')
-        self._temperature = value
+        assert torch.allclose(torch.tensor(1.), torch.tensor(self.loss_weights).sum()), "loss_weights must sum to 1"
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.model_name = model_name
+        self.ce_loss = CrossEntropyLoss()
+        self.kl_loss = KLDivLoss(reduction='batchmean')
+        self.cosine_loss = CosineEmbeddingLoss()
 
     def get_logits_student(self,
-                   input_ids: Tensor,
-                   attention_mask: Tensor,
-                   decoder_input_ids: Tensor,
-                   decoder_attention_mask: Tensor,
-                   **kwargs) -> Tensor:
+                     input_ids: Tensor,
+                        attention_mask: Tensor,
+                        decoder_input_ids: Tensor,
+                        decoder_attention_mask: Tensor,
+                        **kwargs) -> Tensor:
         """
         Get the logits from the student model and the teacher model
         :param input_ids: The input ids
@@ -108,16 +126,16 @@ class Distiller:
                                       attention_mask=attention_mask,
                                       decoder_input_ids=decoder_input_ids,
                                       decoder_attention_mask=decoder_attention_mask,
-                                      **kwargs)[0]
+                                      **kwargs).logits
 
         return student_logits
 
     def get_logits_teacher(self,
-                    input_ids: Tensor,
-                    attention_mask: Tensor,
-                    decoder_input_ids: Tensor,
-                    decoder_attention_mask: Tensor,
-                    **kwargs) -> Tensor:
+                     input_ids: Tensor,
+                        attention_mask: Tensor,
+                        decoder_input_ids: Tensor,
+                        decoder_attention_mask: Tensor,
+                        **kwargs) -> Tensor:
         """
         Get the logits from the student model and the teacher model
         :param input_ids: The input ids
@@ -127,78 +145,89 @@ class Distiller:
         :param kwargs: Additional arguments
         :return: The student logits and the teacher logits
         """
+        self.teacher.eval()
         with torch.no_grad():
             teacher_logits = self.teacher(input_ids=input_ids,
-                                            attention_mask=attention_mask,
-                                            decoder_input_ids=decoder_input_ids,
-                                            decoder_attention_mask=decoder_attention_mask,
-                                            **kwargs)[0]
+                                        attention_mask=attention_mask,
+                                        decoder_input_ids=decoder_input_ids,
+                                        decoder_attention_mask=decoder_attention_mask,
+                                        **kwargs).logits
+
         return teacher_logits
 
-    def get_logits(self,
-                  input_ids: Tensor,
-                  attention_mask: Tensor,
-                  decoder_input_ids: Tensor,
-                  decoder_attention_mask: Tensor,
-                  **kwargs) -> Tuple[Tensor, Tensor]:
-          """
-          Get the logits from the student model and the teacher model
-          :param input_ids: The input ids
-          :param attention_mask: The attention mask
-          :param decoder_input_ids: The decoder input ids
-          :param decoder_attention_mask: The decoder attention mask
-          :param kwargs: Additional arguments
-          :return: The student logits and the teacher logits
-          """
-          student_logits = self.get_logits_student(input_ids=input_ids,
-                                                     attention_mask=attention_mask,
-                                                     decoder_input_ids=decoder_input_ids,
-                                                     decoder_attention_mask=decoder_attention_mask,
-                                                     **kwargs)
-          teacher_logits = self.get_logits_teacher(input_ids=input_ids,
-                                                     attention_mask=attention_mask,
-                                                     decoder_input_ids=decoder_input_ids,
-                                                     decoder_attention_mask=decoder_attention_mask,
-                                                     **kwargs)
-          return student_logits, teacher_logits
-
     def forward(self,
-            input_ids: Tensor,
-            attention_mask: Tensor,
-            decoder_input_ids: Tensor,
-            decoder_attention_mask: Tensor,
-            labels: Tensor,
-    ) -> Tensor:
+                input_ids: Tensor,
+                attention_mask: Tensor,
+                decoder_input_ids: Tensor,
+                decoder_attention_mask: Tensor,
+                **kwargs) -> Tensor:
         """
-        Compute the loss between the student and teacher logits
+        Forward pass through the student model
         :param input_ids: The input ids
         :param attention_mask: The attention mask
         :param decoder_input_ids: The decoder input ids
         :param decoder_attention_mask: The decoder attention mask
-        :param labels: The labels
-        :return: The loss
+        :param kwargs: Additional arguments
+        :return: The student logits
         """
-        student_logits, teacher_logits = self.get_logits(input_ids=input_ids,
-                                                         attention_mask=attention_mask,
-                                                         decoder_input_ids=decoder_input_ids,
-                                                         decoder_attention_mask=decoder_attention_mask)
-        return self.compute_loss(student_logits, teacher_logits, labels)
+        return self.student(input_ids=input_ids,
+                            attention_mask=attention_mask,
+                            decoder_input_ids=decoder_input_ids,
+                            decoder_attention_mask=decoder_attention_mask,
+                            **kwargs)[0]
 
-    def compute_loss(self,
-                student_logits: Tensor,
-                teacher_logits: Tensor,
-                labels: Tensor) -> Tensor:
-            """
-            Compute the loss between the student and teacher logits
-            :param student_logits: The student logits
-            :param teacher_logits: The teacher logits
-            :param labels: The labels
-            :return: The loss
-            """
-            student_logits = (student_logits / self.temperature).softmax(1)
-            teacher_logits = (teacher_logits / self.temperature).softmax(1)
-            CE_loss = nn.CrossEntropyLoss()(student_logits, labels)
-            KD_loss = nn.KLDivLoss()(student_logits.log(), teacher_logits)
-            EM_loss = CosineEmbeddingLoss()(student_logits, teacher_logits, torch.ones_like(labels))
-            loss = self.loss_weights[0] * CE_loss + self.loss_weights[1] * KD_loss + self.loss_weights[2] * EM_loss
-            return loss
+    def training_step(self,
+                        batch: dict,
+                        batch_idx: int,) -> Tensor:
+
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        decoder_input_ids = batch["decoder_input_ids"]
+        decoder_attention_mask = batch["decoder_attention_mask"]
+        # labels = batch["labels"]
+
+        student_logits = self.get_logits_student(input_ids,
+                                                 attention_mask,
+                                                 decoder_input_ids,
+                                                 decoder_attention_mask)
+        teacher_logits = self.get_logits_teacher(input_ids,
+                                                 attention_mask,
+                                                 decoder_input_ids,
+                                                 decoder_attention_mask)
+
+        # Cross entropy loss
+        ce_loss = self.loss_weights[0] * self.ce_loss(student_logits.permute(0, 2, 1), decoder_input_ids)
+
+        # KL divergence loss
+        kl_loss = self.loss_weights[1] * self.kl_loss(student_logits.log_softmax(-1), teacher_logits.softmax(dim=-1))
+
+        # Cosine loss
+        # cosine_loss = self.loss_weights[2] * self.cosine_loss(student_logits, teacher_logits, torch.ones_like(student_logits[:, 0]))
+
+        loss = ce_loss + kl_loss# + cosine_loss
+        self.log("ce_loss", ce_loss)
+        self.log("kl_loss", kl_loss)
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = Adam(self.student.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        return optimizer
+
+    def get_student(self) -> T5ForConditionalGeneration:
+        return self.student
+
+    def get_teacher(self) -> T5ForConditionalGeneration:
+        return self.teacher
+
+    def get_temperature(self) -> float:
+        return self._temperature
+
+    def get_n(self) -> float:
+        return self.n
+
+    def get_loss_weights(self) -> list[float]:
+        return self.loss_weights
+
+    def get_lr(self) -> float:
+        return self.lr
---
+++ distiller/train_distiller.py
@@ -0,0 +1,54 @@
+from Distiller import Distiller
+import transformers
+from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+import sys
+sys.path.append('../')
+from datamodules import MTDistillationDatamodule
+from datasets import load_dataset, load_metric
+from datasets.dataset_dict import DatasetDict
+import argparse
+import wandb
+from transformers import AutoTokenizer
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+
+wandb_logger = WandbLogger(project="distiller", entity="deeplearning2")
+
+teacher_checkpoint = "t5-small"
+
+parser = argparse.ArgumentParser()
+
+metric = load_metric("sacrebleu")
+
+tokenizer = AutoTokenizer.from_pretrained(teacher_checkpoint)
+
+language_codes = ['en-ro', 'en-fr']
+
+max_input_length = 256
+max_target_length = 256
+
+batch_size = 2
+
+dm = MTDistillationDatamodule(batch_size=batch_size, group_pairs=False)
+dm.setup()
+
+distiller = Distiller(
+    teacher=AutoModelForSeq2SeqLM.from_pretrained(teacher_checkpoint),
+    n=1,
+    temperature=1,
+    loss_weights=[1/2, 1/2, 0],
+)
+
+
+
+trainer = pl.Trainer(
+    gpus=1,
+    max_epochs=1,
+    logger=wandb_logger,
+    # precision=16, # lisa fp16
+    # amp_backend="apex", # lisa fp16
+    # amp_level="O2", # lisa fp16
+)
+
+trainer.fit(distiller, dm)
